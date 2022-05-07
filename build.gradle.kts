a00bd90f9e555195578448927a422a7779b9f742
Jacob Lin <jacoblindev@gmail.com>
2022-05-07 15:05:48 +0800

    refactor: remove validation due to not working

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
-	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/jldev/sbkotlindemo/controller/LangController.kt
+++ src/main/kotlin/com/jldev/sbkotlindemo/controller/LangController.kt
@@ -3,18 +3,13 @@ package com.jldev.sbkotlindemo.controller
 import com.jldev.sbkotlindemo.model.Lang
 import com.jldev.sbkotlindemo.service.LangService
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.links.Link
-import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/lang")
